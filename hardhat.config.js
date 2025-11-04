cf2cb697f5b8795670790a85a02147670fc5f56f
madelinebro <mbxkd@umsystem.edu>
2025-11-04 10:39:00 -0600

    mew

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
 module.exports = {
-solidity: "0.8.20",
-networks: {
-didlab: {
-url: process.env.RPC_URL || "https://rpc.blockchain.didlab.org",
-chainId: 252501,
-accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] :
-[],
-},
-},
+  solidity: "0.8.20",
+  networks: {
+    didlab: {
+      url: process.env.RPC_URL || "https://rpc.blockchain.didlab.org",
+      chainId: 252501,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,9 @@
+const hre = require("hardhat");
+async function main() {
+const [deployer] = await hre.ethers.getSigners();
+const Badge = await hre.ethers.getContractFactory("DidLabBadge");
+const badge = await Badge.deploy(deployer.address);
+await badge.waitForDeployment();
+console.log("DidLabBadge:", await badge.getAddress());
+}
+main().catch((e)=>{ console.error(e); process.exit(1); });
